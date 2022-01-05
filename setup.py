4f020ade85510f208e7ba4fd4d8f4f3160f04ab1
Rubens <rubens.sautter@gmail.com>
2022-01-05 12:14:03 -0300

    Packaging

--- setup.py
+++ setup.py
@@ -2,8 +2,10 @@ from setuptools import setup
 
 setup(name="GPA",
       version="4.0",
-      ext_modules=cythonize("*.py"),
       author='Rubens Andreas Sautter',
       author_email='rubens.sautter@gmail.com',
-      url='https://github.com/rsautter/GPA')
+      url='https://github.com/rsautter/GPA',
+      install_requires=['numpy','numba','scipy'],
+      py_modules=['GPA']
+     )
 
