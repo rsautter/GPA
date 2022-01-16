b48b460362206196870861cbc0ec892471604209
Rubens <rubens.sautter@gmail.com>
2022-01-15 23:53:11 -0300

    Solving Wheel problems
    Removed pyproject.toml, as new versions of setuptools already specifies requirements

--- setup.py
+++ setup.py
@@ -1,14 +1,10 @@
-from distutils.core import setup
-from setuptools import find_packages
-
-import numpy
+from setuptools import setup
 
 setup(name="GPA",
       version="4.0",
-      ext_modules=cythonize("*.py"),
       author='Rubens Andreas Sautter',
       author_email='rubens.sautter@gmail.com',
       url='https://github.com/rsautter/GPA',
-      include_dirs=[numpy.get_include()],
-      packages=find_packages())
-
+      install_requires=['numpy','numba','scipy'],
+      py_modules=['GPA2D','GPA3D']
+     )
